649f72cc15f69c7700fab2c55ce530f280c71cb9
Joel Dorantes | Web Developer <47311518+Joel1098@users.noreply.github.com>
2023-09-10 01:37:38 -0600

    Delete practicaja directory

--- practicaja/src/main/java/com/practicaja/services/PracticajaServices.java
+++
@@ -1,9 +0,0 @@
-package com.practicaja.services;
-import org.springframework.stereotype.Service;
-
-import com.practicaja.common.Exception;
-@Service
-public interface PracticajaServices {
-	
-	String saluda() throws Exception;
-}
